088ac950e973e5ded21ce5185a430187eaf3a896
Rakshit <rakshit087@gmail.com>
2022-07-28 01:20:58 +0530

    Added hardhat config

---
+++ hardhat.config.ts
@@ -0,0 +1,27 @@
+import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/types";
+
+dotenv.config();
+
+const primaryWallet: string | null = process.env.WALLET_PRIMARY;
+const secondaryWallet: string | null = process.env.WALLET_SECONDARY;
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      accounts: [
+        {
+          privateKey: primaryWallet,
+          balance: "1000000000000000000000",
+        },
+        {
+          privateKey: secondaryWallet,
+          balance: "1000000000000000000000",
+        },
+      ],
+    },
+  },
+};
+
+export default config;
