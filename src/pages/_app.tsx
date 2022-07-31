4762357199ebc65a23f0d3ae13b180d8eb23eec2
Rakshit <rakshit087@gmail.com>
2022-07-31 20:21:56 +0530

    Added dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
-import "../styles/globals.css";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { MoralisProvider } from "react-moralis";
+import { Navbar } from "../layouts/Navbar";
+import theme from "../utilities/theme";
 
 function MyApp({ Component, pageProps }) {
   const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
   const appId = process.env.NEXT_PUBLIC_APP_ID;
   return (
     <MoralisProvider serverUrl={serverUrl} appId={appId}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Navbar />
+        <Component {...pageProps} />
+      </ChakraProvider>
     </MoralisProvider>
   );
 }
