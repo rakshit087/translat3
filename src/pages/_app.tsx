c7fd8b7523655f32598a5142053f73a23d27d9d2
Rakshit <rakshit087@gmail.com>
2022-07-31 19:46:45 +0530

    Added moralis

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+  const appId = process.env.NEXT_PUBLIC_APP_ID;
+  return (
+    <MoralisProvider serverUrl={serverUrl} appId={appId}>
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
 export default MyApp;
