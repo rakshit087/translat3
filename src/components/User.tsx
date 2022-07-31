6ed032331ddbe4f56ffab4373585289ac0da3b74
Rakshit <rakshit087@gmail.com>
2022-08-01 00:48:36 +0530

    Worked on Auth

---
+++ src/components/User.tsx
@@ -0,0 +1,47 @@
+import { Avatar, Box, Button, Menu, MenuButton, MenuList, MenuItem, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { useMoralis } from "react-moralis";
+export const User = () => {
+  const { authenticate, isAuthenticated, user, isAuthenticating, logout, Moralis } = useMoralis();
+
+  return (
+    <Menu>
+      <MenuButton as={Button} rounded={"full"} cursor={"pointer"} padding={0}>
+        <Avatar size={"sm"} src={"https://avatars.dicebear.com/api/male/username.svg"} />
+      </MenuButton>
+      <MenuList paddingX={2} paddingY={4} textAlign="center">
+        {!isAuthenticated && (
+          <Box>
+            <Text marginBottom={5}>Connect Your Wallet here</Text>
+            <Button
+              colorScheme={"purple"}
+              onClick={async () => {
+                await authenticate({ signingMessage: "Login to Translat3" });
+                Moralis.enableWeb3();
+                Moralis.onAccountChanged(() => {
+                  console.log("Account Cahnged");
+                  logout();
+                });
+              }}
+            >
+              Connect
+            </Button>
+          </Box>
+        )}
+        {isAuthenticated && (
+          <Box>
+            <Text marginBottom={5}>{user!.get("ethAddress")}</Text>
+            <Button
+              colorScheme={"red"}
+              onClick={() => {
+                logout();
+              }}
+            >
+              Logout
+            </Button>
+          </Box>
+        )}
+      </MenuList>
+    </Menu>
+  );
+};
--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,14 +1,26 @@
-import { Box, Button, Flex, useColorMode, useColorModeValue, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, IconButton, Text } from "@chakra-ui/react";
+import { useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { User } from "../components/User";
 
 export const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Box bg={useColorModeValue("gray.100", "gray.900")} px={8}>
-      <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+      <Flex h={20} alignItems={"center"} justifyContent={"space-between"}>
         <Box>
-          <Text size={"lg"}>Translat3</Text>
+          <Text fontSize="3xl">Translat3</Text>
         </Box>
-        <Button onClick={toggleColorMode}>Toggle {colorMode === "light" ? "Dark" : "Light"}</Button>
+        <Box>
+          <IconButton
+            aria-label="theme switcher"
+            icon={colorMode == "dark" ? <MoonIcon /> : <SunIcon />}
+            onClick={toggleColorMode}
+            rounded="full"
+            marginRight={2}
+          />
+          <User />
+        </Box>
       </Flex>
     </Box>
   );
