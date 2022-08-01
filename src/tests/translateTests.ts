d77d3da53b853a4a385fb6c7f467ca8f86435e29
Rakshit <rakshit087@gmail.com>
2022-08-01 23:51:05 +0530

    added test for adding projects

--- src/tests/translateTests.ts
+++ src/tests/translateTests.ts
@@ -1,7 +1,6 @@
 import "@nomiclabs/hardhat-ethers";
 import { ethers } from "hardhat";
 import { expect } from "chai";
-import { ContractFactory } from "ethers";
 
 //Check if contract is deployed
 describe("Translate", () => {
@@ -17,6 +16,32 @@ describe("Translate", () => {
     });
     it("Should be the right owner", async () => {
       expect(await contract.deployTransaction.from).to.equal(primaryWallet.address);
+    });
+  });
+  beforeEach(async () => {
+    await contract.postProject("Project 1", "Description 1", "English", "Hindi", {
+      value: ethers.utils.parseUnits("5", "ether"),
+    });
+    await contract.postProject("Project 2", "Description 2", "English", "Spanish", {
+      value: ethers.utils.parseUnits("1", "ether"),
+    });
+  });
+  describe("Posting Project", () => {
+    it("Project should be posted", async () => {
+      const projectCount = await contract.projectId();
+      expect(projectCount.toString()).to.equal("2");
+    });
+    it("Project should be posted with the right details", async () => {
+      const project = await contract.projects(0);
+      expect(project.title).to.equal("Project 1");
+    });
+    it("Project owner should be the primary wallet", async () => {
+      const project = await contract.projects(0);
+      expect(project.author).to.equal(primaryWallet.address);
+    });
+    it("Project vault should have value", async () => {
+      const project = await contract.projects(0);
+      expect(project.vault.toString()).to.equal("5000000000000000000");
     });
   });
 });
