06b6bbae54ce737d0afcb3312b78449c9cafda5a
Rakshit <rakshit087@gmail.com>
2022-08-02 00:35:39 +0530

    Refactored SC

--- src/contracts/Translate.sol
+++ src/contracts/Translate.sol
@@ -1,20 +1,22 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.9;
 
 contract Translat3 {
   
   uint256 public projectId;
+
   struct Project {
     uint256 id;
-    address author;
+    //0 => at pooling phase, 1 => at translation phase, 2 => finished
+    uint256 status;
+    uint256 vault;
     string title;
     string description;
     string primaryLanguage;
     string translateTo;
-    //0 => at pooling phase, 1 => at translation phase, 2 => finished
-    uint256 status;
-    uint256 vault;
+    address author;
   }
+
   struct Translator {
     address id;
     uint256 vault;
@@ -29,14 +31,13 @@ contract Translat3 {
   mapping(address => Translator) public translators;
   mapping(uint256 => Project) public projects;
 
-  function addProject(
+  function postProject(
     string memory _title,
     string memory _description,
     string memory _primaryLanguage,
     string memory _translateTo
   ) external payable {
     require(msg.value > 0, "Initial funds needed");
-    
     Project memory _project;
     _project.title = _title;
     _project.description = _description;
@@ -48,10 +49,13 @@ contract Translat3 {
     projectId++;
   }
 
+  function getProject(uint256 _id) public view returns (Project memory) {
+    return projects[_id];
+  }
+
   function fundProject(uint256 _projectId) external payable {
-    Project memory _project = projects[_projectId];
-    require(_project.status == 0);
-    _project.vault += msg.value;
+    require(projects[_projectId].status == 0, "Project is not in pooling phase");
+    projects[_projectId].vault += msg.value;
   }
 
   function toTranslationPhase(uint256 _projectId) external {
@@ -61,18 +65,17 @@ contract Translat3 {
   }
 
   function distribute(uint256 _projectId, Distribution memory _distribution) external payable {
-    Project memory _project = projects[_projectId];
-    require(_project.author == msg.sender);
-    require(_project.status == 1);
+    require(projects[_projectId].author == msg.sender);
+    require(projects[_projectId].status == 1);
     uint256 _totalAmount = 0;
     for (uint256 i = 0; i <= _distribution.length; i++) {
       _totalAmount += _distribution.amount[i];
     }
-    require(_totalAmount <= _project.vault);
+    require(_totalAmount <= projects[_projectId].vault);
     for (uint256 i = 0; i <= _distribution.length; i++) {
       translators[_distribution.translators[i]].vault += _distribution.amount[i];
     }
-    _project.vault -= _totalAmount;
-    _project.status = 2;
+    projects[_projectId].vault -= _totalAmount;
+    projects[_projectId].status = 2;
   }
 }
