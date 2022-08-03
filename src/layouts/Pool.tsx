df119dc7ca19ee9c82796c3be542f0dd68d370c5
Rakshit <rakshit087@gmail.com>
2022-08-04 02:00:58 +0530

    Added community pool

---
+++ src/layouts/Pool.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@chakra-ui/react";
+
+export const Pool = () => {
+  return (
+    <Box mx={{ base: "auto", md: "4rem" }}>
+      <p>Community Pools will come here</p>
+    </Box>
+  );
+};
--- src/layouts/SideDrawer.tsx
+++ src/layouts/SideDrawer.tsx
@@ -1,44 +1,55 @@
 import { Box, Flex, IconButton } from "@chakra-ui/react";
 import { useColorModeValue } from "@chakra-ui/react";
-import { MdOutlineHome, MdOutlineTranslate } from "react-icons/md/";
+import { MdPolymer, MdOutlineHome, MdOutlineTranslate } from "react-icons/md/";
 import { AddProject } from "../components/AddProject";
 
 export const SideDrawer = ({ currentLayout, setCurrentLayout }) => {
+  const primaryColor = useColorModeValue("gray.300", "gray.800");
   return (
     <Flex
       bgColor={useColorModeValue("gray.100", "gray.900")}
       w={{ base: "100vw", md: "4rem" }}
       h={{ base: "5rem", md: "90vh" }}
       borderRight="solid 1px"
-      borderRightColor={useColorModeValue("gray.300", "gray.700")}
+      borderRightColor={primaryColor}
       flexDirection="column"
       alignItems="center"
       justifyContent="center"
       position="fixed"
       bottom={0}
     >
+      <IconButton
+        aria-label="translate project"
+        icon={<MdPolymer size={"1.5rem"} />}
+        rounded="full"
+        size="lg"
+        onClick={() => setCurrentLayout("pool")}
+        my={4}
+        bgColor={currentLayout == "pool" ? primaryColor : "transparent"}
+        color={"gray.500"}
+      />
+      <IconButton
+        aria-label="home"
+        icon={<MdOutlineHome size={"1.5rem"} />}
+        rounded="full"
+        size="lg"
+        onClick={() => setCurrentLayout("home")}
+        my={4}
+        bgColor={currentLayout == "home" ? primaryColor : "transparent"}
+        color={"gray.500"}
+      />
+      <IconButton
+        aria-label="translate project"
+        icon={<MdOutlineTranslate size={"1.5rem"} />}
+        rounded="full"
+        size="lg"
+        onClick={() => setCurrentLayout("translate-projects")}
+        my={4}
+        bgColor={currentLayout == "translate-projects" ? primaryColor : "transparent"}
+        color={"gray.500"}
+      />
       <Box textAlign="center">
-        <IconButton
-          aria-label="home"
-          icon={<MdOutlineHome size={"1.5rem"} />}
-          rounded="full"
-          size="lg"
-          onClick={() => setCurrentLayout("home")}
-          my={4}
-          bgColor={currentLayout == "home" ? useColorModeValue("gray.300", "gray.800") : "transparent"}
-          color={useColorModeValue("gray.500", "gray.500")}
-        />
         <AddProject />
-        <IconButton
-          aria-label="translate project"
-          icon={<MdOutlineTranslate size={"1.5rem"} />}
-          rounded="full"
-          size="lg"
-          onClick={() => setCurrentLayout("translate-projects")}
-          my={4}
-          bgColor={currentLayout == "translate-projects" ? useColorModeValue("gray.300", "gray.800") : "transparent"}
-          color={useColorModeValue("gray.500", "gray.500")}
-        />
       </Box>
     </Flex>
   );
