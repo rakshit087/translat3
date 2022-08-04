7eb7d3e46514990e6d1191d6be5010ff8cafca9d
Rakshit <rakshit087@gmail.com>
2022-08-04 05:34:28 +0530

    Fixed errors in wagmi hooks of AddProjectButton

--- src/components/AddProjectButton.tsx
+++ src/components/AddProjectButton.tsx
@@ -5,18 +5,19 @@ import abiJSON from "../hardhat/artifacts/src/hardhat/contracts/Translate.sol/Tr
 
 export const AddProjectButton = ({ data }) => {
   const { data: signer } = useSigner();
-  const { contract } = useContract({
+  const contract = useContract({
     addressOrName: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
     contractInterface: abiJSON.abi,
     signerOrProvider: signer,
   });
+
   return (
     <Button
       colorScheme={"purple"}
       onClick={() => {
         const paragraphs = data.fileContent.split("/\r?\n|\r/g");
         contract.postProject(data.name, data.description, data.languageFrom, data.languageTo, paragraphs, {
-          value: ethers.utils.parseUnits(data.amount, "ether"),
+          value: ethers.utils.parseUnits(data.amount.toString(), "ether"),
         });
       }}
     >
