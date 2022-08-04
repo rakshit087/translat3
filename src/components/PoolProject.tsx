245dae9182be1a630ec13ac6e92fdbfd0e447210
Rakshit <rakshit087@gmail.com>
2022-08-04 19:41:45 +0530

    Frontend for Pool and PoolProject

---
+++ src/components/PoolProject.tsx
@@ -0,0 +1,48 @@
+import { Box, Button, Flex, Text, useColorModeValue } from "@chakra-ui/react";
+import Image from "next/image";
+
+interface datatype {
+  name: string;
+  description: string;
+  languageFrom: string;
+  languageTo: string;
+  pooledAmount: string;
+}
+
+export const PoolProject = (data: datatype) => {
+  const bgColor = useColorModeValue("gray.100", "gray.700");
+  return (
+    <Flex bgColor={bgColor} flexDirection={"column"} height={80} borderRadius="xl" px={8} py={4}>
+      <Flex grow={1} flexDir={"column"}>
+        <Flex flexDirection={"row-reverse"} position="relative" left={5}>
+          <Button disabled colorScheme="purple" variant="solid" size={"sm"} rounded="2xl" marginLeft={2}>
+            100
+          </Button>
+          <Button disabled colorScheme="purple" variant="solid" size={"sm"} rounded="2xl" margin={0}>
+            {data.languageFrom.slice(0, 3)} to {data.languageTo.slice(0, 3)}
+          </Button>
+        </Flex>
+        <Box>
+          <Text fontSize="2xl" fontWeight="bold">
+            {data.name}
+          </Text>
+          <Text as={"i"}>
+            <span
+              style={{
+                fontSize: "35px",
+              }}
+            >
+              {data.description[0]}
+            </span>{" "}
+            {data.description.slice(1)}
+          </Text>
+        </Box>
+      </Flex>
+      <Flex py={1} h={10} alignItems={"center"} justifyContent="space-between" roundedBottom={"xl"}>
+        <Button colorScheme={"purple"} variant="solid" w="100%" rounded={"xl"}>
+          Pool Now
+        </Button>
+      </Flex>
+    </Flex>
+  );
+};
--- src/layouts/Pool.tsx
+++ src/layouts/Pool.tsx
@@ -1,9 +1,37 @@
-import { Box } from "@chakra-ui/react";
+import { Box, SimpleGrid } from "@chakra-ui/react";
+import { PoolProject } from "../components/PoolProject";
 
 export const Pool = () => {
   return (
-    <Box mx={{ base: "auto", md: "4rem" }}>
-      <p>Community Pools will come here</p>
-    </Box>
+    <SimpleGrid minChildWidth={{ base: "16rem", md: "20rem" }} w="100%" spacing={"2.5rem"}>
+      <PoolProject
+        name="The Alchemist"
+        description="A book which tells a journey of a shipherd, Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem."
+        languageFrom="English"
+        languageTo="Hindi"
+        pooledAmount="100"
+      />
+      <PoolProject
+        name="The Alchemist"
+        description="A book which tells a journey of a shipherd, Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem."
+        languageFrom="English"
+        languageTo="Hindi"
+        pooledAmount="100"
+      />
+      <PoolProject
+        name="The Alchemist"
+        description="A book which tells a journey of a shipherd, Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem."
+        languageFrom="English"
+        languageTo="Hindi"
+        pooledAmount="100"
+      />
+      <PoolProject
+        name="The Alchemist"
+        description="A book which tells a journey of a shipherd, Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem."
+        languageFrom="English"
+        languageTo="Hindi"
+        pooledAmount="100"
+      />
+    </SimpleGrid>
   );
 };
