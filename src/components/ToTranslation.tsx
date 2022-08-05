90157311767110429785e5a985d319b5440e7ca5
Rakshit <rakshit087@gmail.com>
2022-08-06 00:44:03 +0530

    User Pooled Component khatam be

---
+++ src/components/ToTranslation.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@chakra-ui/react";
+
+export const ToTranslationButton = () => {
+  return (
+    <Button w={"100%"} colorScheme="purple" variant="solid" size={"sm"} rounded="2xl" margin={0}>
+      To Translation
+    </Button>
+  );
+};
