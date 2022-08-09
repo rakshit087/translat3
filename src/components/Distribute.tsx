1ed69aceab70191abb4dc2c3879e96455fbd2d8f
Rakshit <rakshit087@gmail.com>
2022-08-09 21:32:57 +0530

    Added a distribute button

--- src/components/Distribute.tsx
+++ src/components/Distribute.tsx
@@ -1,8 +1,19 @@
 import { Button } from "@chakra-ui/react";
+import { distribute } from "../services/distributeService";
 
 export const Distribute = () => {
   return (
-    <Button w={"100%"} colorScheme="purple" variant="solid" size={"sm"} rounded="2xl" margin={0} onClick={() => {}}>
+    <Button
+      w={"100%"}
+      colorScheme="purple"
+      variant="solid"
+      size={"sm"}
+      rounded="2xl"
+      margin={0}
+      onClick={() => {
+        distribute();
+      }}
+    >
       Distribute
     </Button>
   );
---
+++ src/services/distributeService.tsx
@@ -0,0 +1,5 @@
+import { ethers } from "ethers";
+import abiJSON from "../hardhat/artifacts/src/hardhat/contracts/Translate.sol/Translat3.json";
+export const distribute = async () => {
+  const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
+};
